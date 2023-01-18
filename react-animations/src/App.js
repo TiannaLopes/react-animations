0485a58264248b08bc69ff1c14570e9f46023f89
Tianna Alina Lopes <tiannaalinalopes@Tiannas-MacBook-Pro.local>
2023-01-18 11:33:25 -0500

    #005 adding component styling disaster

--- react-animations/src/App.js
+++ react-animations/src/App.js
@@ -5,7 +5,7 @@ import Hero from './components/Hero';
 import { SliderData } from './data/SliderData';
 import Dropdown from './components/Dropdown';
 import InfoSection from './components/InfoSection';
-
+import {InfoData} from './data/InfoData';
 function App() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -18,7 +18,7 @@ function App() {
       <Navbar toggle={toggle}/>
       <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Hero slides={SliderData} />
-      <InfoSection />
+      <InfoSection {...InfoData}/>
     </>
   );
 }
---
+++ react-animations/src/data/InfoData.js
@@ -0,0 +1,24 @@
+import ImageOne from '../images/cat1.jpg';
+import ImageTwo from '../images/cat2.jpg';
+
+export const InfoData = 
+    {
+        heading: 'Meet our faovurite Doggos',
+    paragraphOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue. Aliquam purus sit amet luctus. Bibendum est ultricies integer quis auctor. Tellus cras adipiscing enim eu turpis egestas. Mollis nunc sed id semper. In ante metus dictum at. Semper feugiat nibh sed pulvinar proin gravida hendrerit. Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Montes nascetur ridiculus mus mauris vitae ultricies leo. Amet luctus venenatis lectus magna fringilla urna porttitor. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan.',
+    paragraphTwo: 'dipiscing bibendum est ultricies integer quis. Purus in massa tempor nec. Amet volutpat consequat mauris nunc congue nisi vitae suscipit. Neque laoreet suspendisse interdum consectetur libero id faucibus. Fringilla urna porttitor rhoncus dolor. Enim ut tellus elementum sagittis vitae. Sociis natoque penatibus et magnis dis parturient montes. Natoque penatibus et magnis dis parturient montes. Ornare aenean euismod elementum nisi quis eleifend. Orci phasellus egestas tellus rutrum tellus pellentesque eu. Porttitor lacus luctus accumsan tortor posuere. Placerat orci nulla pellentesque dignissim enim sit. Morbi tempus iaculis urna id. Vitae auctor eu augue ut lectus arcu bibendum. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim.',
+    buttonLabel: 'View Dogs',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+}
+    
+export const InfoDataTwo = 
+    {
+        heading: 'Meet our favourite Cats',
+    paragraphOne: 'L dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue. Aliquam purus sit amet luctus. Bibendum est ultricies integer quis auctor. Tellus cras adipiscing enim eu turpis egestas. Mollis nunc sed id semper. In ante metus dictum at. Semper feugiat nibh sed pulvinar proin gravida hendrerit. Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Montes nascetur ridiculus mus mauris vitae ultricies leo. Amet luctus venenatis lectus magna fringilla urna porttitor. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan.',
+    paragraphTwo: 'dndum est ultricies integer quis. Purus in massa tempor nec. Amet volutpat consequat mauris nunc congue nisi vitae suscipit. Neque laoreet suspendisse interdum consectetur libero id faucibus. Fringilla urna porttitor rhoncus dolor. Enim ut tellus elementum sagittis vitae. Sociis natoque penatibus et magnis dis parturient montes. Natoque penatibus et magnis dis parturient montes. Ornare aenean euismod elementum nisi quis eleifend. Orci phasellus egestas tellus rutrum tellus pellentesque eu. Porttitor lacus luctus accumsan tortor posuere. Placerat orci nulla pellentesque dignissim enim sit. Morbi tempus iaculis urna id. Vitae auctor eu augue ut lectus arcu bibendum. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim.',
+    buttonLabel: 'View Dogs',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+    }
